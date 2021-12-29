9d2c7c4ac84403f0868f3c410d9a236e6afb4dbd
Antoine <antoine.graire@gmail.com>
2021-12-29 13:44:06 +0100

    Ressources dynamiques partie 1

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -9,12 +9,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProStageController extends AbstractController
 {
     /**
-     * @Route("/pro/stage", name="pro_stage")
+     * @Route("/", name="prostage_acceuil")
      */
     public function index(): Response
     {
-        return $this->render('pro_stage/index.html.twig', [
-            'controller_name' => 'ProStageController',
-        ]);
+        return $this->render('pro_stage/index.html.twig');
+    }
+
+    /**
+     * @Route("/ressources/345", name="prostage_ressource345")
+     */
+    public function afficherRessourcePeda(): Response
+    {
+        return $this->render('pro_stage/affichageRessource.html.twig', ['idRessource' => 345]);
     }
 }
