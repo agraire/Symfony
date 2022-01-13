f8f8c897b021ffc352eff21e18ba67380888a2aa
Antoine Graire <agraire@iutbayonne.univ-pau.fr>
2022-01-13 19:54:33 +0100

    Amélioration page ProStage

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -5,18 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 use App\Entity\Stage;
+use App\Repository\StageRepository;
+
+use App\Entity\Formation;
+use App\Repository\FormationRepository;
+
+use App\Entity\Entreprise;
+use App\Repository\EntrepriseRepository;
 
 class ProStageController extends AbstractController
 {
     /**
      * @Route("/", name="prostage_accueil")
      */
-    public function index(): Response
+    public function index(StageRepository $repositoryStage): Response
     {
-        //Récupérer le repositiry de l'entité STAGE
-        $repositoryStage = $this->getDoctrine() -> getRepository(Stage::class);
-
         //Récupérer les stages enregistrées en BD
         $stages = $repositoryStage->findAll();
         return $this->render('pro_stage/index.html.twig', ['stages'=>$stages]);
@@ -25,18 +30,39 @@ class ProStageController extends AbstractController
     /**
      * @Route("/filtreParEntreprise", name="prostage_FiltreParEntreprise")
      */
-    public function filtrerParEntreprise(): Response
+    public function filtrerParEntreprise(EntrepriseRepository $repositoryEntreprises): Response
     {
-        return $this->render('pro_stage/filtrerParEntreprise.html.twig');
+        $entreprises = $repositoryEntreprises->findAll();
+        return $this->render('pro_stage/filtrerParEntreprise.html.twig', ['entreprises' => $entreprises]);
     }
 
     /**
-     * @Route("/filtreParFormation", name="prostage_FiltreParFormation")
+     * @Route("/filtreParFormation/{nom}", name="prostage_FiltreParFormation")
      */
-    public function filtrerParFormation(): Response
+    public function filtrerParFormation(FormationRepository $repositoryFormations, $nom): Response
     {
-        return $this->render('pro_stage/filtrerParFormation.html.twig');
+        $formations = $repositoryFormations->findAll();
+        return $this->render('pro_stage/filtrerParFormation.html.twig',['nomFormation' => $formations, 'LaFormation' => $nom]);
     }
+
+    /**
+     * @Route("/stage/{stage}", name="prostage_StageId")
+     */
+    public function stageId(stage $stage): Response
+    {
+        return $this ->render('pro_stage/stageId.html.twig', ['stage' => $stage]);
+    }
+
+
+    /**
+     * @Route("/entreprise/{id}", name="prostage_EntrepriseID")
+     */
+    public function entrepriseId(Entreprise $entreprise):Response
+    {
+        return $this -> render('pro_stage/entrepriseId.html.twig', ['entreprise' => $entreprise]);
+    }
+
+
 
     /**
      * @Route("/stage/{stage}", name="prostage_Stage_Id")
