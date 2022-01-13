ce94e289042494f793d0da8315db7ac1478e9f39
Antoine Graire <agraire@iutbayonne.univ-pau.fr>
2022-01-13 18:00:17 +0100

    Insertion grand nombre de données dans BD

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,8 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Formation;
+use APP\Entity\Stage;
+use App\Entity\Entreprise;
 
 class AppFixtures extends Fixture
 {
@@ -13,16 +15,64 @@ class AppFixtures extends Fixture
         //Création d'un génrateur de données Faker
         $faker = \Faker\Factory::create('fr_FR');
 
-        $nbStages = 15;
-        for($i=0; $i <= $nbStages; $i++)
+        /***************************************
+         *** CREATION DES ENTREPRISES ***
+         **************************************/
+        for ($i = 0; $i<20; $i++)
         {
+            $entreprise = new Entreprise();
+            $entreprise->setNom($faker->company);
+            $entreprise->setActivite($faker->realText());
+            $entreprise->setURLSite($faker->url);
+            $entreprise->setAdresse($faker->address);
 
-        $formation = new Formation();
-        $formation -> setNomLong($faker->sentence($nbWords = 3, $variableNbWords = true));
-        $formation -> setNomCourt($faker->word);
+            $entreprises[] = $entreprise;
+            $manager->persist($entreprise);
+        }
+        
+
+        /***************************************
+         *** CREATION DES FORMATIONS ***
+         **************************************/
+        $modulesFormation = array(
+            "DUT Info" => "Diplôme Universitaire Technologique Informatique",
+            "LP Multimédia" => "Licence Professionelle Multimédia",
+            "DUT IC" => "Diplôme Universitaire Technologique d'Information et Communication"
+        );
+
+        foreach($modulesFormation as $nomC => $nomL)
+        {
+            $formation = new Formation();
+            $formation->setNomCourt($nomC);
+            $formation->setNomLong($nomL);
 
 
-        $manager->persist($formation);
+            /***************************************
+         *** CREATION DES STAGES ***
+         **************************************/
+            for ($numStage = 0; $numStage<10; $numStage++)
+            {
+                $stage = new Stage();
+                $stage->setTitre($faker->realText(140));
+                $stage->setEmail($faker->email);
+                $stage->setDescription($faker->realText());
+                $stage->setDomaine($faker->word);
+
+                //Relation entre Formation et Stage
+                $stage->addFormation($formation);
+
+                //Création d'un nombre aléatoire
+                $numEntreprise = $faker->numberBetween($min =0,$max=19);
+
+                //Relation entre Entreprise et Stage
+                $stage->setEntreprise($entreprises[$numEntreprise]);
+                $entreprises[$numEntreprise]->addStage($stage);
+
+                $manager->persist($stage);
+                $manager->persist($entreprises[$numEntreprise]);
+
+            }
+            $manager->persist($formation);
         }
 
         //Envoyer les données en BD
